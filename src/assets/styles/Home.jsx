4ba0ac30825f04a6feb1ab107565ddb9cd58c31d
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-06 09:51:48 -0300

    style: :lipstick: Adiciona pequena estilização à página home e aos cards

---
+++ src/assets/styles/Home.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const HomeContainer = styled.div`
+  width: 100vw;
+
+  .filter__name {
+    padding: 15px;
+
+    input {
+      padding: 5px;
+    }
+  }
+
+  .films__section {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-content: flex-start;
+    gap: 10px;
+  }
+
+`;
+
+export default HomeContainer;
---
+++ src/components/Card/style.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  background-color: #afeff1;
+  padding: 15px;
+  width: 200px;
+
+  img {
+    width: 170px;
+  }
+
+`;
+
+export default CardContainer;
