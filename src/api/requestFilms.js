d905df79184fd76fee7ac8553d084c72d999c363
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-08 08:44:41 -0300

    feat: :sparkles: Adiciona await na requisição getFIlms

--- src/api/requestFilms.js
+++ src/api/requestFilms.js
@@ -1,9 +1,19 @@
 export const getFilms = async () => {
   try {
     const response = await fetch('https://ghibliapi.herokuapp.com/films');
-    const data = response.json();
+    const data = await response.json();
     return data;
   } catch (error) {
     console.log(error);
   }
-} 
+}
+
+export const getPeople = async (endpoint) => {
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
