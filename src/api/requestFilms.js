f82f09599821aefb9bed633a2e70347244790440
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-10 12:29:00 -0300

    feat: :sparkles: Adiciona função fetch para trazer detalhes de um filme

--- src/api/requestFilms.js
+++ src/api/requestFilms.js
@@ -1,6 +1,18 @@
+const endpointFilms = 'https://ghibliapi.herokuapp.com/films';
+
 export const getFilms = async () => {
   try {
-    const response = await fetch('https://ghibliapi.herokuapp.com/films');
+    const response = await fetch(endpointFilms);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const getFilm = async (id) => {
+  try {
+    const response = await fetch(`${endpointFilms}/${id}`);
     const data = await response.json();
     return data;
   } catch (error) {
@@ -12,6 +24,7 @@ export const getPeople = async (endpoint) => {
   try {
     const response = await fetch(endpoint);
     const data = await response.json();
+    console.log(data, 'data do fetch');
     return data;
   } catch (error) {
     console.log(error);
