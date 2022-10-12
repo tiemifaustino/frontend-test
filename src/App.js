edf3926c7e21dc6d6a84c2d394eb379c123fbb93
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-12 18:15:00 -0300

    feat: :sparkles: Adiciona novas rotas no App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Films from './pages/Films';
 import GlobalStyle from './assets/styles/GlobalStyle';
+import Actors from './pages/Actors';
+import Locations from './pages/Locations';
+import FilmDetails from './components/FilmDetails.jsx';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route exact path="/" element={ <Home /> } />
+        <Route exact path="/films" element={ <Films /> } />
+        <Route exact path="/films/details/:id" element={ <FilmDetails /> } />
+        <Route exact path="/" element={ <Navigate replace to="/films" /> } />
+        <Route exact path="/actors" element={ <Actors /> } />
+        <Route exact path="/locations" element={ <Locations /> } />
       </Routes>
       <GlobalStyle />
     </>
