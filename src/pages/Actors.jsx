88262d54db263376bed7a924d1f7533fd22f2fad
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-13 10:49:18 -0300

    feat: Renderiza atores na tela e cria filtro para buscar atores pelo nome

--- src/pages/Actors.jsx
+++ src/pages/Actors.jsx
@@ -1,21 +1,49 @@
-// import { useEffect, useState } from 'react';
+import { useEffect, useContext, useState } from 'react';
+import ActorsContainer from '../assets/styles/Actors';
 import Navbar from '../components/Navbar';
+import AppContext from '../context/AppContext';
 
 function Actors() {
+  const [filterPersonName, setFilterPersonName] = useState('');
+  const {
+    peopleFilm,
+    fetchPeople,
+  } = useContext(AppContext);
+
+  const handleFilter = ({ target }) => {
+    setFilterPersonName(target.value);
+  };
+
+  useEffect(() => {
+    fetchPeople();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <>
+    <ActorsContainer>
       <Navbar />
       <div className="filter__name">
         <input
           type="text"
           placeholder="Actor's name"
+          value={ filterPersonName }
+          onChange={ handleFilter }
         />
       </div>
 
-      <div className="films__section">
-        Teste Actors
+      <div className="people__section">
+        {
+          peopleFilm
+            && peopleFilm
+              .filter((person) => person.name.toLowerCase().includes(filterPersonName))
+              .map(({ id, name }) => (
+                <div key={ id }>
+                  { name }
+                </div>
+              ))
+        }
       </div>
-    </>
+    </ActorsContainer>
   );
 }
 
